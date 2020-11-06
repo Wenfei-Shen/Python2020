3b88c8108ac7d31bacfe06d8ca46554d0cf9350c
Wenfei Shen <s2067105@ed.ac.uk>
2020-11-06 08:52:58 +0000

    week 7 assessed test num 2

--- Week 7 assessed quizzes.py
+++ Week 7 assessed quizzes.py
@@ -1,17 +1,21 @@
-# 1
-
-def primes_after(n):
-   if n == 1:
-       yield 2
-   else:
-        x = n - 1
-        while True:
-            x += 1
-            for i in range(2, x):
-                if (x % i) == 0:
-                    break
-            else:
-                yield x
-
-
 # 2
+def float_swap(x):
+    x = float(x)
+    y = str(x)
+    l = len(y)
+
+    for i in range(0, l):
+        if y[i] == '.':
+            d = i
+            break
+    a = y[0:d]
+    b = y[(d+1):l]
+    z = [b, '.', a]
+    result = ''.join(z)
+    return result
+
+
+print(float_swap(45.1234))
+print(float_swap(0.1))
+print(float_swap(37))
+print(float_swap(1))
